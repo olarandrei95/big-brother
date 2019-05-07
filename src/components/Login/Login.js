e33b249ba70e082051f130bf31c2aca1097c6d95
Andrei Olar <andrei.olar@visma.com>
2019-05-07 21:15:58 +0300

    Repaired the login screen

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,98 +1,103 @@
 import React, { Component } from 'react';
 
 import {
-    View,
-    Text,
-    Button,
-    TextInput,
-    StyleSheet,
-    TouchableOpacity,
-    Image,
-    KeyboardAvoidingView
-  } from 'react-native';
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView
+} from 'react-native';
 
-  import imageLogo from "../../assets/images/logo.png";
+import imageLogo from '../../assets/images/logo.png';
 
 type Props = {
-    onPress: Function
+  onPress: Function
 };
 
 class Login extends Component<Props> {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            userName: '',
-        };
+  constructor(props) {
+    super(props);
 
-    }
+    this.state = {
+      userName: ''
+    };
+  }
 
-    render() {
-        return (
-        <View style={{flex: 1, paddingHorizontal: 36, flexDirection: 'column', justifyContent: 'center', backgroundColor: '#FFFFFF'}}>
-            <View>
-                
-            </View>
-            <Image source={imageLogo} style={styles.logo} />
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-              <Text style={styles.titleText}>Big Brother</Text>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-                <Text style={styles.subtitleText}> It sees you when you're sleeping</Text>
-            </View>
-            <View style={{flex: 1}}>
-              <TextInput
-                style={{height: 50, fontSize: 20, borderColor: "#d3d3d3", borderWidth: 1, borderRadius: 12, marginBottom: 10}}
-                placeholder="Please enter your name..."
-                onChangeText={(userName) => this.setState({userName})}
-              />
-              <TouchableOpacity style={styles.container} onPress={() => this.props.onPress(this.state.userName)}>
-                <Text style={styles.text}>Continue</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 16,
+          paddingBottom: 24,
+          justifyContent: 'space-between',
+          backgroundColor: '#FFFFFF'
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image
+            source={imageLogo}
+            style={{
+              height: 150,
+              resizeMode: 'contain',
+              alignSelf: 'center'
+            }}
+          />
+
+          <Text
+            style={{ fontSize: 36, fontWeight: 'bold', textAlign: 'center' }}
+          >
+            Big Brother
+          </Text>
+
+          <Text style={{ fontSize: 20, marginTop: 10, textAlign: 'center' }}>
+            It sees you when you're sleeping
+          </Text>
+        </View>
+
+        <View>
+          <TextInput
+            style={{
+              height: 50,
+              fontSize: 20,
+              paddingHorizontal: 10,
+              borderColor: '#d3d3d3',
+              borderWidth: 1,
+              borderRadius: 12,
+              marginBottom: 10
+            }}
+            placeholder="Please enter your name..."
+            onChangeText={userName => this.setState({ userName })}
+          />
+          <TouchableOpacity
+            style={{
+              height: 50,
+              justifyContent: 'center',
+              backgroundColor: '#32CD32',
+              borderRadius: 12,
+              borderWidth: StyleSheet.hairlineWidth,
+              borderColor: 'rgba(255,255,255,0.7)'
+            }}
+            onPress={() => this.props.onPress(this.state.userName)}
+          >
+            <Text
+              style={{
+                color: '#FFFFFF',
+                textAlign: 'center',
+                marginLeft: 10,
+                fontSize: 20
+              }}
+            >
+              Continue
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-    topContainer: {
-        flex: 1,
-        backgroundColor: "#FFFFFF",
-        alignItems: "center",
-        justifyContent: "space-between"
-      },
-    container: {
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#32CD32",
-        marginBottom: 20,
-        paddingVertical: 12,
-        borderRadius: 12,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: "rgba(255,255,255,0.7)",
-      },
-      text: {
-        color: "#FFFFFF",
-        textAlign: "center",
-        marginLeft: 10,
-        fontSize: 20,
-      },
-        titleText: {
-        fontSize: 36,
-        fontWeight: 'bold',
-        },
-        subtitleText: {
-            fontSize: 20,
-            marginBottom: 120          
-        },
-        logo: {
-            flex: 1,
-            width: "100%",
-            resizeMode: "contain",
-            alignSelf: "center"
-          },
-  });
-
-  export default Login;
+export default Login;
