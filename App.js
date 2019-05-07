53330cd42fe5f74064b2632cc80ad3529b6890f1
Andrei Olar <andrei.olar@visma.com>
2019-05-07 11:56:49 +0300

    Get current location

--- App.js
+++ App.js
@@ -7,22 +7,37 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid
+} from 'react-native';
 import MapView from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import DeviceInfo from 'react-native-device-info';
 import { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
 
 const instructions = Platform.select({
-	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-	android:
-		'Double tap R on your keyboard to reload,\n' +
-		'Shake or press menu button for dev menu'
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
 });
 
 type Props = {};
 
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    };
+  }
+
   componentDidMount() {
     const hasLocationPermission = PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -42,7 +57,10 @@ export default class App extends Component<Props> {
 
     Geolocation.getCurrentPosition(
       position => {
-        console.log(position);
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
       },
       error => {
         console.log(error.code, error.message);
@@ -52,32 +70,19 @@ export default class App extends Component<Props> {
   }
 
   render() {
-	  const uniqueId = DeviceInfo.getUniqueID();
-	  return (
-			  <MapView
-				  style={{flex: 1}}
-				  provider={PROVIDER_GOOGLE}
-				  initialRegion={{
-					  latitude: 60.169346,
-					  longitude: 24.934744,
-					  latitudeDelta: 0.04,
-					  longitudeDelta: 0.05
-				  }}
-			  />
-	  );
+    const uniqueId = DeviceInfo.getUniqueID();
+    console.log(this.state);
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.05
+        }}
+      />
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
