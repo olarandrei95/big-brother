1a8ca97ceb604599dddaeaf56e82dfb4ecae33e1
Harry Nystad <harryjnystad@gmail.com>
2019-05-07 13:37:52 +0300

    added iOS support

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -1,84 +1,93 @@
 import React, { Component } from 'react';
-import { PermissionsAndroid } from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
+import DeviceInfo from 'react-native-device-info';
 
 import type { Employee } from '../../types';
 
 type Props = {
-  employees: Array<Employee>
+	employees : Array<Employee>
 };
 
 type State = {
-  latitude: 0,
-  longitude: 0
+	latitude : 0,
+	longitude : 0
 };
 
 class Map extends Component<Props, State> {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      latitude: 0,
-      longitude: 0
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.isAndroid = Platform === 'Android';
 
-  componentDidMount() {
-    const hasLocationPermission = PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Location Permission',
-        message: 'Big Brother wants to know your location',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK'
-      }
-    )
-      .then(() => this.onLocationGranted())
-      .catch(err => console.log(err));
-  }
+		this.state = {
+			latitude: 0,
+			longitude: 0
+		};
+	}
 
-  onLocationGranted() {
-    Geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      },
-      error => {
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 30000, maximumAge: 10000 }
-    );
-  }
+	componentDidMount() {
+		if (this.isAndroid) {
 
-  render() {
-    return (
-      <MapView
-        key="mymapview"
-        style={{ flex: 1 }}
-        provider={PROVIDER_GOOGLE}
-        region={{
-          latitude: this.state.latitude,
-          longitude: this.state.longitude,
-          latitudeDelta: 0.04,
-          longitudeDelta: 0.05
-        }}
-      >
-        {this.props.employees.map(employee => (
-          <Marker
-            key={employee.id}
-            coordinate={{
-              latitude: employee.coordinate.latitude,
-              longitude: employee.coordinate.longitude
-            }}
-            title={employee.name}
-          />
-        ))}
-      </MapView>
-    );
-  }
+			const hasLocationPermission = PermissionsAndroid.request(
+				PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+				{
+					title: 'Location Permission',
+					message: 'Big Brother wants to know your location',
+					buttonNegative: 'Cancel',
+					buttonPositive: 'OK'
+				}
+			)
+				.then(() => this.onLocationGranted())
+				.catch(err => console.log(err));
+		} else {
+			this.onLocationGranted();
+		}
+	}
+
+	onLocationGranted() {
+		Geolocation.getCurrentPosition(
+			position => {
+				console.log(position.coords.latitude + ' ' + position.coords.longitude);
+				this.setState({
+					latitude: position.coords.latitude,
+					longitude: position.coords.longitude
+				});
+			},
+			error => {
+				console.log(error.code, error.message);
+			},
+			{enableHighAccuracy: true, timeout: 30000, maximumAge: 10000}
+		);
+	}
+
+	render() {
+		return (
+			<MapView
+				key="mymapview"
+				style={{flex: 1}}
+				provider={this.isAndroid ? PROVIDER_GOOGLE : PROVIDER_DEFAULT}
+				region={{
+					latitude: this.state.latitude,
+					longitude: this.state.longitude,
+					latitudeDelta: 0.04,
+					longitudeDelta: 0.05
+				}}
+			>
+				{this.props.employees.map(employee => (
+					<Marker
+						key={employee.id}
+						coordinate={{
+							latitude: employee.coordinate.latitude,
+							longitude: employee.coordinate.longitude
+						}}
+						title={employee.name}
+					/>
+				))}
+			</MapView>
+		);
+	}
 }
 
 export default Map;
