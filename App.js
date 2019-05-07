6c3172b024026fc0d404805a15c7ac070259e6e1
Harry Nystad <harryjnystad@gmail.com>
2019-05-07 11:39:43 +0300

    tring to get map to work

--- App.js
+++ App.js
@@ -6,43 +6,53 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import DeviceInfo from 'react-native-device-info';
+import { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+	android:
+		'Double tap R on your keyboard to reload,\n' +
+		'Shake or press menu button for dev menu'
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView provider="google" />
-      </View>
-    );
-  }
+	render() {
+		const uniqueId = DeviceInfo.getUniqueID();
+
+		return (
+			<View style={styles.container}>
+				<MapView
+					provider={PROVIDER_GOOGLE}
+					initialRegion={{
+						latitude: 60.169346,
+						longitude: 24.934744,
+						latitudeDelta: 0.04,
+						longitudeDelta: 0.05
+					}}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
 });
