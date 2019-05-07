3a5700c6297f8478aa4af9057c1fc6dafce8af07
Andrei Olar <andrei.olar@visma.com>
2019-05-07 11:37:58 +0300

    Added Geolocation

--- App.js
+++ App.js
@@ -6,19 +6,48 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid
+} from 'react-native';
 import MapView from 'react-native-maps';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Geolocation from 'react-native-geolocation-service';
 
 type Props = {};
+
 export default class App extends Component<Props> {
+  componentDidMount() {
+    const hasLocationPermission = PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location Permission',
+        message: 'Big Brother wants to know your loccation',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK'
+      }
+    )
+      .then(() => this.onLocationGranted())
+      .catch(err => console.log(err));
+  }
+
+  onLocationGranted() {
+    console.log('here');
+
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 30000, maximumAge: 10000 }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -33,16 +62,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    margin: 10
+  }
 });
